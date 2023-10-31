0adf1e55bf2b1941e97255b201b3b7af2f753b7e
Aljo <aljo.berginc@gmail.com>
2023-10-31 15:52:43 +0100

    lajki in dislajki

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
+interface ApiResponse {
+  url: string;
+  id: number;
+  likes: number;
+  dislikes: number;
+}
+
 export default async function Home() {
   let imageRequest = await fetch(
     "https://challenge.zerodays.dev/api/v1/photos"
@@ -7,10 +14,14 @@ export default async function Home() {
   let images = await imageRequest.json();
   return (
     <div className="container">
-      {images.map((image) => {
+      {images.map((image: ApiResponse) => {
         return (
           <div key={image.id}>
             <Image src={image.url} height={300} width={500} alt="Image" />
+            <div className="flex items-center justify-between">
+              <span>{image.likes}</span>
+              <span>{image.dislikes}</span>
+            </div>
           </div>
         );
       })}
